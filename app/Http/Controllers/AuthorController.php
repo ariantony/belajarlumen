77f62a2a01d0a1b380e23872f3e1dd26e31353af
Tony Arianto <ariantony@gmail.com>
2020-01-28 00:12:18 +0700

    menambahkan fungsi delete

--- app/Http/Controllers/AuthorController.php
+++ app/Http/Controllers/AuthorController.php
@@ -6,6 +6,7 @@ use App\Author;
 use App\Traits\ApiResponser;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
 
 class AuthorController extends Controller
 {
@@ -38,17 +39,38 @@ class AuthorController extends Controller
 
     public function show($author)
     {
-        
+       $author = Author::findOrFail($author);
+       return $this->successResponse($author);
     }
 
     public function update(Request $request, $author)
     {
+        $rules =[
+            'name' => 'max:255',
+            'gender' => 'max:255|in:male,female',
+            'country' => 'max:255',
+        ];
         
+        $this->validate($request, $rules);
+        $author = Author::findOrFail($author);
+        $author->fill($request->all());
+        
+        if ($author->isClean()) {
+            return $this->errorResponse('At least one value must change', Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+
+        $author->save();
+
+        return $this->successResponse($author);
+
     }
 
     public function destroy($author)
     {
-        
+        $author = Author::findOrFail($author);
+        $author->delete();
+
+        return $this->successResponse($author);
     }
 
 }
